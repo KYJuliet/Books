b42957bd9c09ef4f257cc9aa5bb53565a2d4b5b6
KYJuliet <kevin.yj.yu@gmail.com>
2017-07-09 17:05:57 -0400

    Add files via upload

--- Gutenberg Script.R
+++ Gutenberg Script.R
@@ -1,10 +1,26 @@
 library(dplyr)
 library(tidyr)
+library(tidytext)
+library(ggplot2)
 library(gutenbergr)
+
+
+
+
+
+#---------------------#
+##### EXPLORATION #####
+#---------------------#
+
+### ...Attempt 1 ####
 
 #pull all book metadata that is english and has text (can be downloaded from project gutenberg)
 temp_1 <- gutenberg_metadata %>%
   filter(language == "en", has_text == TRUE)
+
+#first attempt at finding a suitable bookshelf directed me to the "Bestsellers, American, 1895-1923" bookshelf
+#A collection of just under 100 works form the time period. However this led to less than ideal results with much
+#text being housekeeping items and not enough actual text
 
 #find bookshelfs with at least 100 books and arrange in descending order
 temp_2 <- data.frame(table(temp_1$gutenberg_bookshelf)) %>%
@@ -22,3 +38,88 @@ download_list <- temp_1 %>%
 books <- gutenberg_download(download_list, strip = TRUE)
 
 write.csv(books, file = "books.csv")
+
+
+
+### ...Attempt 2 ####
+
+temp_1 <- gutenberg_metadata %>%
+  filter(language == "en", has_text == TRUE)
+
+#903 unique bookshelves but some are sub-categories of others, deliminated by "/"
+length(unique(temp_1$gutenberg_bookshelf))
+#Attempt to extract base bookshelves only by removing all text after a "/"
+temp_2 <- temp_1 %>%
+  mutate(bookshelf = gsub("/\\S*", "", gutenberg_bookshelf))
+#Only 588 bookshelfs now but 3/4 are NA's (30704/41452)
+length(unique(temp_2$bookshelf))
+#Let's see what has a lot of books
+temp_3 <- data.frame(table(temp_2$bookshelf)) %>%
+  arrange(desc(Freq))
+head(temp_3, 50)
+
+#reveiwing the list of the top 50 by number of books, lets go with the following bookshelves
+#Any fiction, Bestsellers, American, 1895-1923, Best Books Ever Listings, and Harvard Classics
+
+#filter for the above bookshelves, select the id column only (to pass to gutenberg_download), and group_by to remove
+# duplicates if any (spoiler: there weren't)
+temp_4 <- temp_2 %>%
+  filter(grepl("Fiction|fiction|Bestsellers, American, 1895-1923|Best Books Ever Listings|Harvard Classics", bookshelf)) %>%
+  select(gutenberg_id) %>%
+  group_by(gutenberg_id) %>%
+  filter(!(gutenberg_id %in% c(19506, 19513, 19515, 19574, 19796)))
+
+books <- gutenberg_download(temp_4, strip = TRUE)
+
+write.csv(books, file = "books.csv")
+
+
+
+#---------------------#
+###### OLD SCRIPT #####
+#---------------------#
+
+df <- read.csv("books.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
+#creating a new unique id var that is seperate from the gutenberg_id variable
+id_table <- data.frame(unique(df$gutenberg_id)) %>%
+  mutate(book_id = row_number()) %>%
+  rename(gutenberg_id = unique.df.gutenberg_id.)
+
+books <- df %>%
+  select(gutenberg_id, text) %>%
+  filter( text != "") %>%
+  
+  #saving a variable for the line number of each word
+  group_by(gutenberg_id) %>%
+  mutate(line = row_number()) %>%
+  ungroup() %>%
+  
+  #matching unique id for the table from 'id_table', seperate from gutenberg_id
+  full_join(id_table, by = "gutenberg_id") %>%
+  select(book_id, gutenberg_id, line, text) %>%
+  arrange(book_id) %>%
+  
+  #unnesting the individual words
+  unnest_tokens(word, text) %>%
+  
+  #creating a new var for the word's position in it's book (identified with book_id)
+  group_by(book_id) %>%
+  mutate(posn = row_number()/n()) %>%
+  mutate(posn_decile = ceiling(posn * 10)/10) %>%
+  ungroup()
+
+#finding the mean position of each word and its number of occurences from 'books'
+words <- books %>%
+  group_by(word) %>%
+  summarize(avg_posn = mean(posn), n = n()) %>%
+  filter(n > 1350) %>%
+  arrange(desc(avg_posn))
+
+head(words, 10)
+tail(words, 10)
+
+#boxplot of frequencies of 'words$n' (the count of number of instances of each word in 'books')
+ggplot(data = words, aes(x = "", y = n)) +
+  geom_boxplot() +
+  coord_cartesian(ylim = c(0, 20))
